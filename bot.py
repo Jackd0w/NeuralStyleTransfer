75fac2dcaaf11a0ff52c24ce5fa6df6ebfbdd8e2
Jackd0w <62934328+Jackd0w@users.noreply.github.com>
2023-03-08 20:31:42 +0300

    added send_photo function

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from configs import TOKEN
 
+#from style_transfer import 
+import aiogram
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -7,6 +9,32 @@ from aiogram.utils import executor
 bot = Bot(token=TOKEN)
 
 dp = Dispatcher(bot)
+
+class NeuralTransferCaller():
+    def __init__(self, ):
+        pass 
+
+    async def get_image_style(self, message: types.Message):
+        photos = message.photo
+
+        for photo in photos:
+            await photo.download()
+
+            #function add to database
+
+
+    async def get_image_transfer(self, message: types.Message):
+        photos = message.photo
+
+        for photo in photos:
+            await photo.download()
+
+            #function add to database
+
+    def send_new_image(self, image. chat_id):
+        photo = bot.send_photo(chat_id=CHAT_ID, photo=#get_from_db)
+
+
 
 @dp.message_handlers(commands=['start'])
 async def welcome(message: types.Message):
